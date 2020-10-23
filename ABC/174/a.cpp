156d2497d0293e09daa465d9e15420b4b25a1c14
exag <hukuti145@gmail.com>
2020-10-23 22:50:32 +0900

    abc174 a

---
+++ ABC/174/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int x;
+  string ans;
+  cin >> x;
+  if (x >= 30) {
+    ans = "Yes";
+  } else {
+    ans = "No";
+  }
+  cout << ans << endl;
+  return 0;
+}

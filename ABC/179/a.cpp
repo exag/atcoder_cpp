4935d5e8b88a8f6ecdc7a7c6d0a7d1929a89e5f4
exag <hukuti145@gmail.com>
2020-10-24 18:14:11 +0900

    anc179 a editorial

--- ABC/179/a.cpp
+++ ABC/179/a.cpp
@@ -7,9 +7,9 @@ using P = pair<int, int>;
 int main() {
   string s;
   cin >> s;
-  if (s[s.size() - 1] == 's')
-    cout << s + "es";
+  if (s.back() == 's')
+    cout << s + "es" << endl;
   else
-    cout << s + "s";
+    cout << s + "s" << endl;
   return 0;
 }

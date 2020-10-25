ee894f3774f2ff704476aa3c3c95534fdee873d0
exag <hukuti145@gmail.com>
2020-10-25 13:11:04 +0900

    abc132 a editorial

--- ABC/132/a.cpp
+++ ABC/132/a.cpp
@@ -7,9 +7,8 @@ using P = pair<int, int>;
 int main() {
   string s;
   cin >> s;
-  vector<char> cs = {s[0], s[1], s[2], s[3]};
-  sort(cs.begin(), cs.end());
-  if (cs[0] == cs[1] && cs[1] != cs[2] && cs[2] == cs[3])
+  sort(s.begin(), s.end());
+  if (s[0] == s[1] && s[1] != s[2] && s[2] == s[3])
     cout << "Yes" << endl;
   else
     cout << "No" << endl;

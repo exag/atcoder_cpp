280f7a5988930025ebdb4e7ada1a92b4bcd16647
exag <hukuti145@gmail.com>
2020-10-25 15:57:30 +0900

    abc128 a

---
+++ ABC/128/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int a, p;
+  cin >> a >> p;
+  cout << (p + 3 * a) / 2 << endl;
+  return 0;
+}

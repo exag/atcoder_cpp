2e3a376bec1c26d0e990e6a3f225d7e1df9529b3
exag <hukuti145@gmail.com>
2020-10-25 16:44:28 +0900

    abc171 a editorial

---
+++ ABC/171/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  char c;
+  cin >> c;
+  if (islower(c))
+    cout << "a" << endl;
+  else
+    cout << "A" << endl;
+  return 0;
+}

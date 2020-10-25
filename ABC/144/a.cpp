e846695ac55a315849f1d186e424582f68fb35d0
exag <hukuti145@gmail.com>
2020-10-25 12:08:32 +0900

    abc144 a

---
+++ ABC/144/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  if (a > 9) {
+    cout << -1 << endl;
+    return 0;
+  }
+  if (b > 9) {
+    cout << -1 << endl;
+    return 0;
+  }
+  cout << a * b << endl;
+  return 0;
+}

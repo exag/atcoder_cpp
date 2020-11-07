f2d6fb4e1b9f5722b35594e850c6513c6a4ea703
exag <hukuti145@gmail.com>
2020-11-07 22:50:09 +0900

    abc132 c

---
+++ ABC/132/c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> d(n);
+  rep(i, n) cin >> d[i];
+  sort(d.begin(), d.end());
+  int l = d[n / 2 - 1];
+  int r = d[n / 2];
+  cout << r - l << endl;
+  return 0;
+}

7cd83dfa216fa50901d7a6766e8579ffb5e715cf
exag <hukuti145@gmail.com>
2020-11-13 22:15:24 +0900

    abc019 a

---
+++ ABC/019/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  vector<int> a(3);
+  cin >> a[0] >> a[1] >> a[2];
+  sort(a.begin(), a.end());
+  cout << a[1] << endl;
+  return 0;
+}

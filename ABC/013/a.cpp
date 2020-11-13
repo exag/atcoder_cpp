db697fea5ea4b93fd16894a0123d48326f1f14f3
exag <hukuti145@gmail.com>
2020-11-13 22:00:01 +0900

    abc013 a

---
+++ ABC/013/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  char x;
+  cin >> x;
+  cout << x - 'A' + 1 << endl;
+  return 0;
+}

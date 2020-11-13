14b6a7412f2cc858a035ed0bb50b30bca7d8fc8c
exag <hukuti145@gmail.com>
2020-11-13 22:15:30 +0900

    abc020 a

---
+++ ABC/020/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int q;
+  cin >> q;
+  map<int, string> ans;
+  ans[1] = "ABC";
+  ans[2] = "chokudai";
+  cout << ans[q] << endl;
+  return 0;
+}

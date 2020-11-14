fb2ef25fbd749903789a8ebfc7881c004e0384c0
exag <hukuti145@gmail.com>
2020-11-15 00:34:14 +0900

    abc099 a

---
+++ ABC/099/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n >= 1000)
+    cout << "ABD" << endl;
+  else
+    cout << "ABC" << endl;
+  return 0;
+}

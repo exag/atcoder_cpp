83c0921143c235da5e4797b60d5f708ba4f85d5c
exag <hukuti145@gmail.com>
2020-11-19 21:49:04 +0900

    abc049 b

---
+++ ABC/ABC049/b/b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  rep(i, h) {
+    string s;
+    cin >> s;
+    cout << s << endl;
+    cout << s << endl;
+  }
+  return 0;
+}

7e8a7ee91234983a714d4e4f28eef6f8e83d884f
exag <hukuti145@gmail.com>
2020-11-20 23:38:38 +0900

    abc007 b

---
+++ ABC/ABC007/b/b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  string a;
+  cin >> a;
+  if (a == "a")
+    cout << -1 << endl;
+  else
+    cout << "a" << endl;
+  return 0;
+}

8bb267900cd1f475a4f66c39f2500708a24b1626
exag <hukuti145@gmail.com>
2020-12-14 20:56:09 +0900

    abc185 a

---
+++ ABC/ABC118/c/c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repex(i, s, n) for (int i = s; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  int ans = a[0];
+  rep(i, n) ans = gcd(ans, a[i]);
+  cout << ans << endl;
+  return 0;
+}

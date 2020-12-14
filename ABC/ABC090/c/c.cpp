ed91bb0900a00437eeed5eadae5c124cf0f46d9c
exag <hukuti145@gmail.com>
2020-12-14 23:12:19 +0900

    abc090 c

--- ABC/ABC090/c/c.cpp
+++ ABC/ABC090/c/c.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repex(i, s, n) for (int i = s; i < (n); ++i)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-int solve() {
-  int n, m;
+ll solve() {
+  ll n, m;
   cin >> n >> m;
-  if (n < 3 && m < 3) return 0;
-  return max(n - 2, 1) * max(m - 2, 1);
+  if (n == 1 && m == 1) return 1;
+  if (n == 1 && m > 1) return m - 2;
+  if (n > 1 && m == 1) return n - 2;
+  return (n - 2) * (m - 2);
 }
 
 int main() {

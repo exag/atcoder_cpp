8f1dfa6408c5291f04ce30fbad712b26f8495bd2
exag <hukuti145@gmail.com>
2020-12-18 23:11:12 +0900

    abc154 d editorial

--- ABC/ABC154/d/d.cpp
+++ ABC/ABC154/d/d.cpp
@@ -5,28 +5,24 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-double f(double x) {
-  double ret;
-  ret = x * (x + 1) / 2.0;
-  ret /= x;
-  return ret;
-}
-
 int main() {
   int n, k;
   cin >> n >> k;
-  vector<int> p(n);
-  rep(i, n) cin >> p[i];
-  vector<double> e(n);
-  rep(i, n) e[i] = f(p[i]);
-  double now = 0;
+  vector<int> e(n);
+  rep(i, n) {
+    int p;
+    cin >> p;
+    // 期待値を2倍しておく
+    e[i] = p + 1;
+  }
+  int now = 0;
   rep(i, k) now += e[i];
-  double ans = now;
+  int ans = now;
   srep(i, 1, n - k + 1) {
     now -= e[i - 1];
     now += e[i + k - 1];
     ans = max(ans, now);
   }
-  printf("%.10f\n", ans);
+  printf("%.10f\n", ans / 2.0);
   return 0;
 }

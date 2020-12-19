5b762b2b3a5555e52d51960d2db5a1db7be5c663
exag <hukuti145@gmail.com>
2020-12-19 23:35:19 +0900

    abc186 c

---
+++ ABC/ABC186/c/c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define srep(i, s, n) for (int i = s; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int check7(int n, int t) {
+  while (n) {
+    int m = n % t;
+    if (m == 7) return false;
+    n /= t;
+  }
+  return true;
+}
+
+int main() {
+  int n, w;
+  cin >> n >> w;
+  int ans = 0;
+  srep(i, 1, n + 1) {
+    if (check7(i, 10) && check7(i, 8)) ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}

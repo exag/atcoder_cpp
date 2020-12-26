2a1b66ca49e2205fd9012cf18ffc1a5e4206af4d
exag <hukuti145@gmail.com>
2020-12-26 22:37:50 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,17 +1,11 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define srep(i, s, n) for (int i = s; i < (n); ++i)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
-template <class T>
-void chmax(T& a, T b) {
-  if (a < b) a = b;
-}
-template <class T>
-void chmin(T& a, T b) {
-  if (a > b) a = b;
-}
 
 int main() {
   //

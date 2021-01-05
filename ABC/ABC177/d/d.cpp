104bd79c62b407e5083bfd505c6ee784809a8b00
exag <hukuti145@gmail.com>
2021-01-06 06:39:00 +0900

    abc177 d editorial

---
+++ ABC/ABC177/d/d.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define srep(i, s, n) for (int i = s; i < (n); ++i)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+// UnionFind
+// coding: https://youtu.be/TdR816rqc3s?t=726
+// comment: https://youtu.be/TdR816rqc3s?t=6822
+struct UnionFind {
+  vector<int> d;
+  UnionFind(int n = 0) : d(n, -1) {}
+  int find(int x) {
+    if (d[x] < 0) return x;
+    return d[x] = find(d[x]);
+  }
+  bool unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return false;
+    if (d[x] > d[y]) swap(x, y);
+    d[x] += d[y];
+    d[y] = x;
+    return true;
+  }
+  bool same(int x, int y) { return find(x) == find(y); }
+  int size(int x) { return -d[find(x)]; }
+};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  UnionFind uf(n);
+  rep(i, m) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    uf.unite(a, b);
+  }
+  int ans = 0;
+  rep(i, n) chmax(ans, uf.size(i));
+  cout << ans << endl;
+  return 0;
+}

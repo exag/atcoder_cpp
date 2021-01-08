88fe8395899ea5a3d9fb5b8b2405243874b2f018
exag <hukuti145@gmail.com>
2021-01-08 21:59:56 +0900

    abc171 e

---
+++ ABC/ABC171/e/e.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define srep(i, s, n) for (int i = s; i < (n); ++i)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  int tot = 0;
+  rep(i, n) tot ^= a[i];
+  rep(i, n) cout << (tot ^ a[i]) << endl;
+  return 0;
+}

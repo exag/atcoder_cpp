510509013f27b238692cc815fd9787f68a777a1a
exag <hukuti145@gmail.com>
2021-01-16 22:15:28 +0900

    abc174 d editorial2

--- ABC/ABC174/c/c.cpp
+++ ABC/ABC174/c/c.cpp
@@ -11,16 +11,12 @@ int main() {
   int k;
   cin >> k;
   int x = 7 % k;
-  set<int> s;
-  int i = 1;
-  while (s.count(x) == 0) {
+  for (int i = 1; i <= k; ++i) {
     if (x == 0) {
       cout << i << endl;
       return 0;
     }
-    s.insert(x);
     x = (x * 10 + 7) % k;
-    i++;
   }
   cout << -1 << endl;
   return 0;

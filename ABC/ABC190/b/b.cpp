f61700baa3619a7f4cf6f05d4156438db504191e
exag <hukuti145@gmail.com>
2021-02-20 17:43:20 +0900

    abc190 b

---
+++ ABC/ABC190/b/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define srep(i, s, n) for (int i = s; i < (n); ++i)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n, s, d;
+  cin >> n >> s >> d;
+  string ans = "No";
+  rep(i, n) {
+    int x, y;
+    cin >> x >> y;
+    if (x < s && y > d) ans = "Yes";
+  }
+  cout << ans << endl;
+  return 0;
+}

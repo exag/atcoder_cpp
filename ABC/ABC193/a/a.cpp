60ba5defe8d5594bf354348448d4200d131230ba
exag <hukuti145@gmail.com>
2021-03-13 18:46:23 +0900

    abc193 a

---
+++ ABC/ABC193/a/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define srep(i, s, n) for (int i = s; i < (n); ++i)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  double a, b;
+  cin >> a >> b;
+  double p = (1 - b / a) * 100;
+  printf("%.20f\n", p);
+  return 0;
+}
